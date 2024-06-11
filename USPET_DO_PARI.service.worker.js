ed10c0b64eef7626f5ace38d6f7795e0831c29ae
Александр <a.varnakin@g.nsu.ru>
2024-06-11 22:06:21 +0700

    v1.2.1

--- USPET_DO_PARI.service.worker.js
+++ USPET_DO_PARI.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1718117959|1714000631";
+const CACHE_VERSION = "1718118328|2083568084";
 const CACHE_PREFIX = "USPET DO PARI-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "USPET_DO_PARI.offline.html";
